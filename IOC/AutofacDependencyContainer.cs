2bfb97f1a7e99aea6ac725a00981343e560a7e44
nishad5089 <nishad5089@gmail.com>
2019-09-29 22:27:10 +0600

    AutoFacUpdate

--- IOC/AutofacDependencyContainer.cs
+++ IOC/AutofacDependencyContainer.cs
@@ -1,7 +1,26 @@
+using System;
+using Application;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace IOC
 {
     public class AutofacDependencyContainer
     {
+        public static IServiceProvider RegisterServices(IServiceCollection services)
+        {
+            // Now register our services with Autofac container.
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new ApplicationModule());
+            builder.RegisterModule(new InfrastructureModule());
+            builder.RegisterModule(new InfrastructureModule());
+            builder.Populate(services);
+            var container = builder.Build();
+            // Create the IServiceProvider based on the container.
+            return new AutofacServiceProvider(container);
 
+        }
     }
 }
